2bcac37a4da2b43888d7a4576e25303732c9353a
MISSOULA\mf217126 <mariah.forbes@umontana.edu>
2017-04-20 14:05:56 -0600

    changes for the punching

--- mockups/Gravity_Knights/public/js/OtherPlayer.js
+++ mockups/Gravity_Knights/public/js/OtherPlayer.js
@@ -4,7 +4,9 @@ class OtherPlayer {
     constructor() {
         // Foot not head
         this.pos = createVector(800 / 2, 600 - 100);
-        this.attackpos = createVector(this.pos.x, this.pos.y)
+        this.attackpos = createVector(this.pos.x, this.pos.y);
+        this.attackvel = 0;
+        this.attackacc = 10;
         this.attacksize = {
             w: 25,
             h: 25
@@ -14,6 +16,7 @@ class OtherPlayer {
         this.gravity = 1;
         this.ground = 600 - 100;
         this.velocity = 0;
+        this.isattacking = false;
 
         // size should be 0-100
         this.size = 100;
