54cb76f97b4929bbc78d9b7157f2a1780931373c
Chey Bear <tonywithaq@gmail.com>
2017-04-25 12:22:34 -0600

    fixed punching :}

--- mockups/Gravity_Knights/public/js/OtherPlayer.js
+++ mockups/Gravity_Knights/public/js/OtherPlayer.js
@@ -5,8 +5,6 @@ class OtherPlayer {
         // Foot not head
         this.pos = createVector(800 / 2, 600 - 100);
         this.attackpos = createVector(this.pos.x, this.pos.y);
-        this.attackvel = 0;
-        this.attackacc = 10;
         this.attacksize = {
             w: 25,
             h: 25
@@ -16,7 +14,6 @@ class OtherPlayer {
         this.gravity = 1;
         this.ground = 600 - 100;
         this.velocity = 0;
-        this.isattacking = false;
 
         // size should be 0-100
         this.size = 100;
