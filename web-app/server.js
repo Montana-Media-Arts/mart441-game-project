febd1ee2929d0b315e63792875be1e97263ac9ba
Michael Musick <michael@michaelmusick.com>
2017-05-08 13:49:25 -0600

    cleaned up server file

--- web-app/server.js
+++ web-app/server.js
@@ -18,24 +18,27 @@ app.use(express.static("./public"));
 
 io.on("connection", function(socket) {
     var myIdx = num_of_players;
-	  var myIdxStr = myIdx.toString();
+    var myIdxStr = myIdx.toString();
     num_of_players++;
 
 
     console.log("A user connected.");
     socket.emit("Welcome", "Hello, from the server.");
     socket.emit("player_num", myIdx);
-	socket.emit("initialize_others", players_idx);
+    socket.emit("initialize_others", players_idx);
 
-	// Join their own individual room.
-		// This allows us to address them individually.
-	socket.join(myIdxStr);
+    // Join their own individual room.
+    // This allows us to address them individually.
+    socket.join(myIdxStr);
 
-	players_idx.push(myIdxStr);
+    players_idx.push(myIdxStr);
 
-	player_hits[myIdxStr] = 0;
+    player_hits[myIdxStr] = 0;
 
-    io.emit('newplayer', {thereidx: myIdx, players: players_idx} );
+    io.emit('newplayer', {
+        thereidx: myIdx,
+        players: players_idx
+    });
 
 
 
@@ -45,24 +48,24 @@ io.on("connection", function(socket) {
         player_data[myIdxStr] = playerData;
     });
 
-	// Incoming hit data
-	socket.on('player hit', function(hitPlayer) {
-		player_hits[hitPlayer]++;
-		console.log('hit player', hitPlayer, player_hits);
-		// io.emit('hit player', {thereidx: myIdx, players: players_idx});
-		io.in(hitPlayer).emit('hit player', "You were hit - " + hitPlayer);
-});
+    // Incoming hit data
+    socket.on('player hit', function(hitPlayer) {
+        player_hits[hitPlayer]++;
+        console.log('hit player', hitPlayer, player_hits);
+        // io.emit('hit player', {thereidx: myIdx, players: players_idx});
+        io.in(hitPlayer).emit('hit player', "You were hit - " + hitPlayer);
+    });
 
     // Cleanup
     socket.on("disconnect", function() {
         console.log("User disconnected: " + myIdx);
-		var idx2remove = players_idx.indexOf( myIdxStr );
-		players_idx.splice( idx2remove, 1 );
-		console.log(myIdx);
-		console.log(players_idx);
+        var idx2remove = players_idx.indexOf(myIdxStr);
+        players_idx.splice(idx2remove, 1);
+        console.log(myIdx);
+        console.log(players_idx);
 
-		delete player_data[myIdxStr];
-		delete player_hits[myIdxStr];
+        delete player_data[myIdxStr];
+        delete player_hits[myIdxStr];
         io.emit("disconnect_player", myIdx);
     });
 
@@ -72,7 +75,7 @@ io.on("connection", function(socket) {
 var sendLoop = setInterval(function() {
 
         io.emit("player_data", player_data);
-		// console.log(player_data);
+        // console.log(player_data);
     },
     20
 );
